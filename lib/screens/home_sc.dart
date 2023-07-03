73b90835f448dba50ad5d412ecac46fbf641c845
Taha Sabiri <121357512+Taha-sabiri@users.noreply.github.com>
2023-07-03 22:08:08 +0330

    newpage -1.1

--- lib/screens/home_sc.dart
+++ lib/screens/home_sc.dart
@@ -4,6 +4,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:hamedanconcert/model/concert.dart';
 import 'package:hamedanconcert/model/news.dart';
+import 'package:hamedanconcert/screens/news_sc.dart';
 
 class Homescreen extends StatelessWidget {
   const Homescreen({super.key});
@@ -73,98 +74,105 @@ class Homescreen extends StatelessWidget {
                     itemBuilder: (context, index) {
                       return Padding(
                         padding: const EdgeInsets.only(left: 10),
-                        child: Container(
-                          height: 340,
-                          width: 250,
-                          child: Stack(children: [
-                            Container(
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(20),
-                                image: DecorationImage(
-                                    image: AssetImage(news[index].image),
-                                    fit: BoxFit.cover),
+                        child: InkWell(
+                          onTap: () {
+                            Navigator.of(context).push(MaterialPageRoute(
+                                builder: (context) => NewsSc(index: index)));
+                          },
+                          child: Container(
+                            height: 340,
+                            width: 250,
+                            child: Stack(children: [
+                              Container(
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(20),
+                                  image: DecorationImage(
+                                      image: AssetImage(news[index].image),
+                                      fit: BoxFit.cover),
+                                ),
                               ),
-                            ),
-                            Positioned(
-                              bottom: 0,
-                              left: 0,
-                              right: 0,
-                              child: ClipRRect(
-                                borderRadius: BorderRadius.only(
-                                    bottomLeft: Radius.circular(20),
-                                    bottomRight: Radius.circular(20)),
-                                child: BackdropFilter(
-                                  filter: ImageFilter.blur(
-                                      sigmaX: 10.0, sigmaY: 10.0),
-                                  child: Container(
-                                    height: 150,
-                                    width: 250,
-                                    decoration: BoxDecoration(
-                                        color: Color.fromARGB(75, 0, 0, 0)),
-                                    child: Padding(
-                                      padding: const EdgeInsets.all(8.0),
-                                      child: Column(
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.start,
-                                        children: [
-                                          Text(
-                                            news[index].headertit,
-                                            style: TextStyle(
-                                                color: Colors.white,
-                                                fontSize: 15,
-                                                fontFamily: 'Kalame',
-                                                fontWeight: FontWeight.w700),
-                                          ),
-                                          SizedBox(
-                                            height: 10,
-                                          ),
-                                          Text(
-                                            news[index].tit,
-                                            style: TextStyle(
-                                                color: Colors.white,
-                                                fontSize: 13,
-                                                fontFamily: 'Kalame',
-                                                fontWeight: FontWeight.w400),
-                                          ),
-                                          SizedBox(
-                                            height: 15,
-                                          ),
-                                          Row(
-                                            mainAxisAlignment:
-                                                MainAxisAlignment.spaceBetween,
-                                            children: [
-                                              Row(
-                                                children: [
-                                                  Icon(
-                                                    CupertinoIcons.clock,
-                                                    color: Colors.white,
-                                                    size: 20,
-                                                  ),
-                                                  Text(
-                                                    news[index].time,
-                                                    style: TextStyle(
-                                                        color: Colors.white,
-                                                        fontSize: 13,
-                                                        fontFamily: 'Kalame',
-                                                        fontWeight:
-                                                            FontWeight.w400),
-                                                  )
-                                                ],
-                                              ),
-                                              Icon(
-                                                Icons.arrow_forward,
-                                                color: Colors.white,
-                                              ),
-                                            ],
-                                          ),
-                                        ],
+                              Positioned(
+                                bottom: 0,
+                                left: 0,
+                                right: 0,
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.only(
+                                      bottomLeft: Radius.circular(20),
+                                      bottomRight: Radius.circular(20)),
+                                  child: BackdropFilter(
+                                    filter: ImageFilter.blur(
+                                        sigmaX: 10.0, sigmaY: 10.0),
+                                    child: Container(
+                                      height: 150,
+                                      width: 250,
+                                      decoration: BoxDecoration(
+                                          color: Color.fromARGB(75, 0, 0, 0)),
+                                      child: Padding(
+                                        padding: const EdgeInsets.all(8.0),
+                                        child: Column(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          children: [
+                                            Text(
+                                              news[index].headertit,
+                                              style: TextStyle(
+                                                  color: Colors.white,
+                                                  fontSize: 15,
+                                                  fontFamily: 'Kalame',
+                                                  fontWeight: FontWeight.w700),
+                                            ),
+                                            SizedBox(
+                                              height: 10,
+                                            ),
+                                            Text(
+                                              news[index].tit,
+                                              style: TextStyle(
+                                                  color: Colors.white,
+                                                  fontSize: 13,
+                                                  fontFamily: 'Kalame',
+                                                  fontWeight: FontWeight.w400),
+                                            ),
+                                            SizedBox(
+                                              height: 15,
+                                            ),
+                                            Row(
+                                              mainAxisAlignment:
+                                                  MainAxisAlignment
+                                                      .spaceBetween,
+                                              children: [
+                                                Row(
+                                                  children: [
+                                                    Icon(
+                                                      CupertinoIcons.clock,
+                                                      color: Colors.white,
+                                                      size: 20,
+                                                    ),
+                                                    Text(
+                                                      news[index].time,
+                                                      style: TextStyle(
+                                                          color: Colors.white,
+                                                          fontSize: 13,
+                                                          fontFamily: 'Kalame',
+                                                          fontWeight:
+                                                              FontWeight.w400),
+                                                    )
+                                                  ],
+                                                ),
+                                                Icon(
+                                                  Icons.arrow_forward,
+                                                  color: Colors.white,
+                                                ),
+                                              ],
+                                            ),
+                                          ],
+                                        ),
                                       ),
                                     ),
                                   ),
                                 ),
-                              ),
-                            )
-                          ]),
+                              )
+                            ]),
+                          ),
                         ),
                       );
                     }),
---
+++ lib/screens/news_sc.dart
@@ -0,0 +1,170 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:hamedanconcert/model/news.dart';
+
+class NewsSc extends StatelessWidget {
+  final int index;
+  const NewsSc({super.key, required this.index});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(automaticallyImplyLeading: false, title: Row()),
+      body: Stack(
+        children: [
+          Positioned(
+            top: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              foregroundDecoration: BoxDecoration(
+                  gradient: LinearGradient(colors: [
+                const Color.fromARGB(0, 0, 0, 0),
+                Colors.black,
+              ], end: Alignment.bottomCenter, begin: Alignment.topCenter)),
+              width: double.infinity,
+              height: 435,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      fit: BoxFit.cover, image: AssetImage(news[index].image))),
+            ),
+          ),
+          Positioned(
+            right: 15,
+            top: 280,
+            child: Text(
+              news[index].headertit,
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontFamily: 'Kalame',
+                  fontWeight: FontWeight.w700),
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              width: double.infinity,
+              height: 500,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(47), color: Colors.white),
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 15, right: 15),
+                    child: Row(
+                      children: [
+                        Container(
+                          width: 50,
+                          height: 50,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(7),
+                              image: DecorationImage(
+                                  image: AssetImage('assets/images/icon.png'))),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(right: 10),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "همدان کنسرت",
+                                style: TextStyle(
+                                    fontFamily: 'Kalame',
+                                    fontWeight: FontWeight.w700,
+                                    fontSize: 13),
+                              ),
+                              Text(
+                                "علیرضا رحمانیان",
+                                style: TextStyle(
+                                    fontFamily: 'Kalame',
+                                    fontWeight: FontWeight.w400,
+                                    fontSize: 13),
+                              ),
+                            ],
+                          ),
+                        ),
+                        SizedBox(
+                          width: 80,
+                        ),
+                        SizedBox(
+                          height: 37,
+                          width: 80,
+                          child: Container(
+                            alignment: Alignment.center,
+                            decoration: BoxDecoration(
+                                border: Border.all(
+                                  width: 1,
+                                  color: Color.fromRGBO(21, 70, 248, 1),
+                                ),
+                                borderRadius: BorderRadius.circular(7)),
+                            child: Row(
+                              children: [
+                                Icon(
+                                  Icons.bookmark_outline_outlined,
+                                  color: Color.fromRGBO(21, 70, 248, 1),
+                                ),
+                                Text(
+                                  "ذخیره کنید",
+                                  style: TextStyle(
+                                      fontSize: 10,
+                                      color: Color.fromRGBO(21, 70, 248, 1),
+                                      decoration: TextDecoration.none,
+                                      fontFamily: 'Kalame',
+                                      fontWeight: FontWeight.w700),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        SizedBox(
+                          height: 37,
+                          width: 37,
+                          child: Container(
+                            alignment: Alignment.center,
+                            decoration: BoxDecoration(
+                                color: Color.fromRGBO(21, 70, 248, 1),
+                                borderRadius: BorderRadius.circular(7)),
+                            child: Icon(
+                              Icons.send,
+                              color: Colors.white,
+                              size: 15,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 15, right: 15),
+                    child: SizedBox(
+                      child: Text(news[index].tit,
+                          style: TextStyle(
+                              fontSize: 15,
+                              color: Color.fromRGBO(56, 56, 56, 1),
+                              decoration: TextDecoration.none,
+                              fontFamily: 'Kalame',
+                              fontWeight: FontWeight.w300)),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
